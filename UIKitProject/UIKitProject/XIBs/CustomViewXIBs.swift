a9255337deb5f8f633db8579ab4691d3e730c403
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-01-23 13:59:51 -0500

    Navegar de un Storyboard a un XIB

---
+++ UIKitProject/UIKitProject/XIBs/CustomViewXIBs.swift
@@ -0,0 +1,20 @@
+//
+//  CustomViewXIBs.swift
+//  UIKitProject
+//
+//  Created by YeltsinMacPro13 on 23/01/23.
+//
+
+import UIKit
+
+class CustomViewXIBs: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UIKitProject/UIKitProject/XIBs/ViewControllerD.swift
@@ -0,0 +1,23 @@
+//
+//  ViewControllerD.swift
+//  UIKitProject
+//
+//  Created by YeltsinMacPro13 on 23/01/23.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerD: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func didTapOnButton() {
+//        nib: utiliza la maquina para entender el codigo
+        let viewControllerXIBs = ViewControllerXIBs(nibName: "ViewControllerXIBs", bundle: nil)
+        self.present(viewControllerXIBs, animated: true)
+    }
+    
+}
